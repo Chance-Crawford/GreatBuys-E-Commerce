1dac85f277e4f8dbf7ee4fe183c559f744e47e14
Chance <chancecrawford30@gmail.com>
2022-01-29 19:50:37 -0800

    created tag and product tag models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,28 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // foreign key for product 
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    // foreign key for tag
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
