8c0b5f7ea58600b3516d5468420a821512ee3033
Chance <chancecrawford30@gmail.com>
2022-01-30 19:19:26 -0800

    created all tag routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
+// if someone calls any route that does not exist on the server,
+// send this as the default
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
 });
